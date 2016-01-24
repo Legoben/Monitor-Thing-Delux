7afb878d7a7ab71c6d202181ed0221dddf380570
Tristan Seifert <tristan@squee.co>
2016-01-23 22:35:23 -0500

    A spectre is haunting Europe — the spectre of communism. All the powers of old Europe have entered into a holy alliance to exorcise this spectre: Pope and Tsar, Metternich and Guizot, French Radicals and German police-spies.
    Where is the party in opposition that has not been decried as communistic by its opponents in power? Where is the opposition that has not hurled back the branding reproach of communism, against the more advanced opposition parties, as well as against its reactionary adversaries?

--- app/scripts/ui/ATMCard.js
+++ app/scripts/ui/ATMCard.js
@@ -2,8 +2,37 @@
 'use strict';
 var React = require('react');
 
+// lmao
+var If = React.createClass({
+	render: function() {
+		if (this.props.test) {
+			return this.props.children;
+		}
+		else {
+			return false;
+		}
+	}
+});
+
 // view for rendering ATM card
 var ATMCard = React.createClass({
+	// calculate the average thing
+	componentWillReceiveProps: function(newProperties) {
+		// calculate the average of the star ratings
+		var reviews = newProperties.reviews;
+		var numReviews = 0, runningAvg = 0;
+
+		for(review in reviews) {
+			runningAvg += review.rating;
+			numReviews++;
+		}
+
+		this.setState({
+			avgRating: (runningAvg / numReviews),
+			hasReviews: (reviews.length != 0)
+		});
+	},
+
 	render: function() {
 		return (
 			<div className="row" id="index-banner">
@@ -20,19 +49,23 @@ var ATMCard = React.createClass({
 							<div className="atm-amount">
 								<span>${this.props.atm.amount_left}</span>
 							</div>
-							<span className="card-title">
-								<div className="rating">
-									<img src="images/star.svg" alt="Star 1" />
-									<img src="images/star.svg" alt="Star 2" />
-									<img src="images/star.svg" alt="Star 3" />
-									<img src="images/star-half.svg" alt="Star 4" />
-									<img src="images/star-outline.svg" alt="Star 5" />
-								</div>
-							</span>
+							<If test={this.props.hasReviews}>
+								<span className="card-title">
+									<div className="rating">
+										<img src="images/star.svg" alt="Star 1" />
+										<img src="images/star.svg" alt="Star 2" />
+										<img src="images/star.svg" alt="Star 3" />
+										<img src="images/star-half.svg" alt="Star 4" />
+										<img src="images/star-outline.svg" alt="Star 5" />
+									</div>
+								</span>
+							</If>
 							<p>
 								<a href="#">Write a review (IMPLEMENT THIS)</a>
 							</p>
 						</div>
+
+						<!-- this shit is in the card actually -->
 						<div className="card-reveal">
 							<span className="card-title grey-text text-darken-4">
 								<span className="review-title">Reviews</span>
